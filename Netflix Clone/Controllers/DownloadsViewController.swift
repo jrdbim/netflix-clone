027e161e635d98ce8b786d28d1c45e9be38093a1
Jiradet Amornpimonkul <jiradet.amorn@gmail.com>
2023-04-08 00:29:55 -0400

    First Commit

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by Jiradet Amornpimonkul on 4/7/23.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ Netflix Clone/MainTabBarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Jiradet Amornpimonkul on 4/7/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .blue
+        
+        let homeVC = UINavigationController(rootViewController: HomeViewController())
+        let upcomingVC = UINavigationController(rootViewController: UpcomingViewController())
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        let downloadsVC = UINavigationController(rootViewController: DownloadsViewController())
+        
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        upcomingVC.tabBarItem.image = UIImage(systemName: "play.circle")
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadsVC.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        homeVC.title = "Home"
+        upcomingVC.title = "Upcoming"
+        searchVC.title = "Search"
+        downloadsVC.title = "Downloads"
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([homeVC, upcomingVC, searchVC, downloadsVC], animated: true)
+    }
+
+
+}
+
